6875299c699f6b39993a8091b744d7615434f381
SwaroopReddyGottigundala <60024334+SwaroopReddyGottigundala@users.noreply.github.com>
2020-03-02 11:43:40 -0600

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,20 @@
+function restoGrandtotal()
+{
+     let rIntialname=parseFloat(document.querySelector("name").value)
+     
+         
+     
+     //let rTax=rIntialTotal*(rTaxRate/100)
+    // let rnamecount=rIntialTotal.length;
+
+     
+    // document.getElementById("Tax").innerHTML = rTax.toFixed(2);
+     document.getElementById("namecount").innerHTML = rIntialname.length  
+
+
+
+
+
+
+
+}
